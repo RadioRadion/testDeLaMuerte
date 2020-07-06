05581b32f8bd2df314e7c3a042d21b0d428d3536
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-07-06 11:13:34 +0200

    function best results added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,7 +4,8 @@ class GamesController < ApplicationController
     @game = Game.new
     @party = Party.new
     frenchDic
-    compare(@file)
+    @bestResults = compare(@dictionnary, @letters)
+    raise
   end
 
   private
@@ -27,26 +28,37 @@ class GamesController < ApplicationController
 
   def frenchDic
     path = File.join(Rails.root, 'lib', 'data', 'liste_francais.txt')
-    @file = []
+    @dictionnary = []
     #encoding with utf-8caracters
     File.open(path, 'rb', encoding: "ISO8859-1:utf-8").each do |line|
       if (line.length < 10 && !line.include?("-"))
       #line bien cheloue pour gsub tous les caractères latins spéciaux en pas spéciaux
-        @file << line.mb_chars.normalize(:kd).gsub(/[^\x00-\x7F]/n,'').upcase.to_s.strip
+        @dictionnary << line.mb_chars.unicode_normalize(:nfkc).gsub(/[^\x00-\x7F]/n,'').upcase.to_s.strip
       end
     end
-    @file
+    @dictionnary
   end
-######### ça commence à péter sur cette fonction !!!!!!!!!!!!
-  def compare(file)
-    allResults = []
-    n = 10
-    until n < 1
-      @letters.permutation(n).to_a.each do |possibility|
-        file.include?(possibility) ? allResults << possibility
-        allResults.length > 10 ? break
-      end
-      n -= 1
+
+  def compare(dictionnary, letters)
+    results = []
+    dictionnary.each do |word|
+      pickLetters = letters.map { |letter| letter }
+      word.chars.each { |letter| pickLetters.include?(letter) ? pickLetters.delete_at(pickLetters.index(letter)) : false }
+      results << word if word.length == (letters.length - pickLetters.length)
     end
+    return results.sort{|x, y| x.length <=> y.length}.last(10)
   end
+
 end
+  ######première fonction compare qui ne marche qu'avec un ordinateur quantique
+  # def compare(dictionnary)
+  #   allResults = []
+  #   n = 10
+  #   until n < 1
+  #     @letters.permutation(n).to_a.each do |possibility|
+  #       allResults << possibility if dictionnary.include?(possibility.join)
+  #       break allResults if allResults.length > 9
+  #     end
+  #     n -= 1
+  #   end
+  # end
